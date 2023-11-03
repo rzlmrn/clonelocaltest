f750dcad67e8c0c77675ab77b59d0898787e746e
rzlmrn <rizalnurhandaru@gmail.com>
2023-11-03 22:13:46 +0700

    menambahkan text baru

--- flocal.cpp
+++ flocal.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     cout << "membuat clone dari lokal" << endl;
     cout << "ini file clone yang dibuat dari lokal" << endl;
+    cout << "anda dapat menghubungkannya dengan github" << endl;
     return 0;
 }
